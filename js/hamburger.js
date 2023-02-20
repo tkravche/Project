d294f289b8d10d1709994cfba0e5b37f609738a6
Tanya <tkravche@yahoo.com>
2023-02-20 15:14:23 +0100

    Final chnages

---
+++ js/hamburger.js
@@ -0,0 +1,8 @@
+
+var hamburger = getQuerySelector('.hamburger');
+var menu = getQuerySelector('.hamburger');
+hamburger.addEventListener('click', function () {
+    menu.classList.tooggle('is-active');
+})
+
+ 
